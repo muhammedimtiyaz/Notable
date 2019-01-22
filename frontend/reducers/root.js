06f109141faf002126ae6a8c5372b393707c7028
Muhammed Imtiyaz <muhammedimtiyaz87@gmail.com>
2019-01-22 17:56:27 -0500

    add store

--- frontend/reducers/root.js
+++ frontend/reducers/root.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import entitiesReducer from "./entities";
+// import entitiesReducer from "./entities";
 import sessionReducer from "./sessions_reducer";
 
 export default combineReducers({
