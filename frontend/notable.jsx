564d21995fd5a309a392a2415cd8b8333c6f24da
Muhammed Imtiyaz <muhammedimtiyaz87@gmail.com>
2019-01-22 13:54:38 -0500

    finish configuration

---
+++ frontend/notable.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoader", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Hello</h1>, root);
+});
