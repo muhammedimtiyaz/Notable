e08b83c9cc14d90c720e36a2f543fe03b787b906
Muhammed Imtiyaz <muhammedimtiyaz87@gmail.com>
2019-02-06 17:32:23 -0500

    fix typo

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,58 +12,58 @@ class Api::UsersController < ApplicationController
     else
       render json: @user.errors.full_messages, status: 422
     end
+  end
 
-    def show
-      @user = User.where(:id => current_user.id).first
-      render :show
-    end
+  def show
+    @user = User.where(:id => current_user.id).first
+    render :show
+  end
 
-    def get_favorites
-      if current_user
-        favourites = Favourites.where(:id => current_user.id)
+  def get_favorites
+    if current_user
+      favourites = Favourites.where(:id => current_user.id)
 
-        favourite_restaurants = {}
-        favourites.each do |favourite|
-          restaurant = Restaurant.where(:id => restaurant_id).first
-          favourite_restaurants[restaurant.id] = restaurant unless restaurant.nil?
-        end
-        render json: favourite_restaurants
-      else
-        render json: {}
-    end
-
-    def add_favourite_restaurant
-      if current_user
-        @favourite = Favourites.where(:user_id => current_user.id).where(:restaurant_id => params[:restaurantId]).first
-
-        if @favourite.nil?
-          @favourite.restaurant_id = params[:restaurantId]
-          @favourite.user_id = current_user.id
-          @favourite.save!
-        end
-
-        render json: Restaurant.where(:id => @favourite.restaurant_id).first
-      else
-        render json: {}
+      favourite_restaurants = {}
+      favourites.each do |favourite|
+        restaurant = Restaurant.where(:id => restaurant_id).first
+        favourite_restaurants[restaurant.id] = restaurant unless restaurant.nil?
       end
-    end
+      render json: favourite_restaurants
+    else
+      render json: {}
+  end
 
-    def remove_favourite
-      if current_user
-        @favourite = Favourites.where(:user_id => current_user.id).where(:restaurant_id => params[:restaurantId]).first
+  def add_favourite_restaurant
+    if current_user
+      @favourite = Favourites.where(:user_id => current_user.id).where(:restaurant_id => params[:restaurantId]).first
 
-        if @favourite
-          @favourite.destroy!
-        end
-
-        render json: @favourite.restaurant_id
-      else
-        render json: {}
+      if @favourite.nil?
+        @favourite.restaurant_id = params[:restaurantId]
+        @favourite.user_id = current_user.id
+        @favourite.save!
       end
+
+      render json: Restaurant.where(:id => @favourite.restaurant_id).first
+    else
+      render json: {}
     end
   end
 
-  private
+  def remove_favourite
+    if current_user
+      @favourite = Favourites.where(:user_id => current_user.id).where(:restaurant_id => params[:restaurantId]).first
+
+      if @favourite
+        @favourite.destroy!
+      end
+
+      render json: @favourite.restaurant_id
+    else
+      render json: {}
+    end
+  end
+
+private
 
   def user_params
     params.require(:user).permit(:firstname, :lastname, :username, :email, :password)
